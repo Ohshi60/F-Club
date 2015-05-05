612826de47d09d5e46c6bf7ca0bede8cc61ff0c2
Ohshi60 <lakseborgen@gmail.com>
2015-05-05 17:46:26 +0200

    Version 1
    Vi har fået email validering og lidt af user klassen op at køre!!! op på
    hesten knægt

---
+++ F-Club/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace F_Club
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            User u1 = new User("Birthe", "Neger", "mail@hej.dk");
+            User u2 = new User("HonningMelone", "StorePatværk", "store@bryster.dk");
+            Console.WriteLine("User:  " + u1.ToString());
+            Console.ReadKey();
+        }
+    }
+}
---
+++ F-Club/User.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+
+namespace F_Club
+{
+    class User
+    {
+        private int memberCount = 0;       
+        private int _userID;
+        private string _firstName;
+        private string _lastName;
+        private string _userName;
+        private string _email;
+        private int _balance; // Vi definerer balancen i ører og kan derfor arbejde med en INT
+
+        public User(string fname, string lname, string email)
+        {
+            _balance = 0;
+            memberCount++;
+            _userID = memberCount;
+            if(fname != null)
+                _firstName = fname;
+            if(lname != null)
+                _lastName = lname;
+            if(emailValidator(email) == true)
+                _email = email;
+        }
+
+        public bool emailValidator(string email)
+        {
+            string pattern1 = @"^[a-zA-Z0-9-\\._]+@[a-zA-Z0-9][A-Za-z0-9-].+[a-zA-Z0-9]+$";
+            if(Regex.IsMatch(email, pattern1) == true)
+                return true;
+            else
+                return false;
+        }
+
+        public override string ToString()
+        {
+            string userinfo = _firstName + " " + _lastName + " " + _email;
+            return userinfo;
+        }
+    }
+}
